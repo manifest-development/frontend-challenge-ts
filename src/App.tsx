2dc9102d8dbd32f6f24e7364d61fefcf154a43fd
gz244 <gz244@cornell.edu>
2024-01-30 20:12:29 -0500

    add a thankyou page with responsive designs

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { FormContext } from './context/FormProvider'
 import FormNavBar from './components/form-nav-bar'
 import FormContent from './components/form-content'
 import FullPageLoader from './components/full-page-loader'
+import ThankYouPage from './components/thank-you-page/ThankYouPage'
 
 function App () {
   const {
@@ -12,20 +13,33 @@ function App () {
     updateUserData,
     confirmForm,
     backToPreviousStep,
-    isLoading
+    isLoading,
+    thankYouPage,
+    endThankYou
   } = useContext(FormContext)
 
   return (
     <div className="app">
       {isLoading && <FullPageLoader />}
-      <FormNavBar currentStep={formStep} />
-      <FormContent
-        currentStep={formStep}
-        updateUserData={updateUserData}
-        userData={userData}
-        confirmForm={confirmForm}
-        backToPreviousStep={backToPreviousStep}
-      />
+      {!thankYouPage
+        ? (
+          <div>
+            <FormNavBar currentStep={formStep} />
+            <FormContent
+              currentStep={formStep}
+              updateUserData={updateUserData}
+              userData={userData}
+              confirmForm={confirmForm}
+              backToPreviousStep={backToPreviousStep}
+            />
+          </div>
+          )
+        : (<ThankYouPage
+              thankyou={thankYouPage}
+              userName={userData.name}
+              endThankYou={endThankYou}
+            />)
+      }
     </div>
   )
 }
