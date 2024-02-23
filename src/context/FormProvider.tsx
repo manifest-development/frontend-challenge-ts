fe02ad967ee93d663a11f7ac263e24e9a7702db9
Ritwik Vaidya <vaidyaritwik@gmail.com>
2024-02-23 02:08:38 -0500

    Complete exercise 2

--- src/context/FormProvider.tsx
+++ src/context/FormProvider.tsx
@@ -5,11 +5,13 @@ import React, {
   useState
 } from 'react'
 import { User } from '../class/user'
+import { useNavigate } from 'react-router-dom'
 
 const CreateProviderValue = () => {
   const [formStep, setFormStep] = useState<number>(1)
   const [userData, setUserData] = useState<User>(new User())
   const [isLoading, setIsLoading] = useState<boolean>(false)
+  const navigate = useNavigate()
 
   const updateFormStep = (updatedUserData: User) => {
     if (updatedUserData.name && updatedUserData.income >= 0) {
@@ -31,21 +33,26 @@ const CreateProviderValue = () => {
       setTimeout(res, 1500)
     })
 
-  const saveAndResetData = async () => {
+  const resetData = async () => {
     try {
-      await mockSaveData()
-      // ADD THANK YOU PAGE HERE
       setUserData(new User())
       setFormStep(1)
       setIsLoading(false)
+      navigate('/')
     } catch (e) {
       console.error(e)
     }
   }
 
-  const confirmForm = () => {
+  const confirmForm = async () => {
     setIsLoading(true)
-    saveAndResetData()
+    try {
+      await mockSaveData()
+      // ADD THANK YOU PAGE HERE
+      navigate('/thank-you')
+    } catch (e) {
+      console.error(e)
+    }
   }
 
   const backToPreviousStep = () => {
@@ -59,6 +66,7 @@ const CreateProviderValue = () => {
     updateUserData,
     confirmForm,
     backToPreviousStep,
+    resetData,
     isLoading
   }
 }
---
+++ src/pages/ThankYouPage/ThankYouPage.tsx
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react'
+
+import Button from '../../components/button'
+import { FormContext } from '../../context/FormProvider'
+import Container from '../../layout/container'
+import * as Styled from './styles'
+import KatieImage from '../../assets/katie.png'
+
+const ThankYouPage: React.FC = () => {
+  const { resetData, userData } = useContext(FormContext)
+  return (
+    <Container>
+      <Styled.ThankYouPageContainer>
+        <div>
+          <img src={KatieImage} alt="Katie" />
+          <p>Hi, {userData.name}, thank you for submitting the form. We will check and get back to you within 2 business days</p>
+        </div>
+        {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
+        <Button onClick={resetData} id="submit-another-button">Submit Another</Button>
+      </Styled.ThankYouPageContainer>
+    </Container>
+  )
+}
+
+export default ThankYouPage
