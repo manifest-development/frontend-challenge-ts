bb43f5ac6f507f83f925fe8065e407d9e40c2b60
Bart <bartoszswierzynski7@gmail.com>
2024-03-09 13:19:59 -0600

    fixed validation issue of not being able to remove the first character of the name

--- src/utils/validateNameInput.ts
+++ src/utils/validateNameInput.ts
@@ -1,4 +1,7 @@
 const isNameInputValid = (value: string): boolean => {
+  // If the value is empty, consider it valid to allow the user to delete the entire input
+  if (value === "") return true;
+
   const isInputLettersSpacesPeriod: boolean = /^[A-Za-z. ]+$/.test(value);
 
   if (isInputLettersSpacesPeriod) return true;
