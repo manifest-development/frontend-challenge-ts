6dcea0d85b4d42b4494fea9a7aeb8f7c3c0033a4
Bart <bartoszswierzynski7@gmail.com>
2024-03-09 00:48:37 -0600

    Updated capitalizeName utility function to ensure all characters after the first letter were lowercased to ensure it was displayed correctly

--- src/utils/capitalizeName.ts
+++ src/utils/capitalizeName.ts
@@ -4,12 +4,12 @@ const capitalizeName = (name: string) => {
   if (isFullName) {
     return name
       .split(" ")
-      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
       .join(" ");
   }
 
   // First Name Only
-  return name.charAt(0).toUpperCase() + name.slice(1);
+  return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
 };
 
 export default capitalizeName;
